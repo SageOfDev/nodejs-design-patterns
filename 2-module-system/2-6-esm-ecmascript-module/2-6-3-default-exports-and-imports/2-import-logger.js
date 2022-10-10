79323dcfa920fe170a82a4cdf7065e61a39066e5
sj <bcj0114@gmail.com>
2022-10-10 18:46:55 +0900

    + 2-6-3 p71

---
+++ 2-module-system/2-6-esm-ecmascript-module/2-6-3-default-exports-and-imports/2-import-logger.js
@@ -0,0 +1,3 @@
+import MyLogger from './1-logger.js'
+const logger = new MyLogger('info')
+logger.log('Hello World')
---
+++ 2-module-system/2-6-esm-ecmascript-module/2-6-3-default-exports-and-imports/3-show-default.js
@@ -0,0 +1,4 @@
+// import * as loggerModule from './1-logger.js'
+// console.log(loggerModule)
+
+// import {default} from './1-logger.js'
