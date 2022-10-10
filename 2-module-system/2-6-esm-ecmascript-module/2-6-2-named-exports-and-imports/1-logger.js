18080318c7d086381c62f0e143b2821676ad40dc
sj <bcj0114@gmail.com>
2022-10-10 18:25:49 +0900

    + 2-6-2 p69

---
+++ 2-module-system/2-6-esm-ecmascript-module/2-6-2-named-exports-and-imports/1-logger.js
@@ -0,0 +1,24 @@
+export function log(message) {
+  console.log(message)
+}
+
+export const DEFAULT_LEVEL = 'info'
+
+export const LEVEL = {
+  error: 0,
+  debug: 1,
+  warn: 2,
+  data: 3,
+  info: 4,
+  verbose: 5
+}
+
+export class Logger {
+  constructor(name) {
+    this.name = name
+  }
+
+  log(message) {
+    console.log(`[${this.name}] ${message}`)
+  }
+}
---
+++ 2-module-system/2-6-esm-ecmascript-module/2-6-2-named-exports-and-imports/2-import-logger.js
@@ -0,0 +1,19 @@
+// import * as loggerModule from './1-logger.js'
+// console.log(loggerModule)
+
+// import {log} from './1-logger.js'
+// log('Hello World')
+
+// import {log, Logger} from './1-logger.js'
+// log('Hello World')
+// const logger = new Logger('DEFAULT')
+// logger.log('Hello World')
+
+// import {log} from './1-logger.js'
+// const log = console.log
+
+import {log as log2} from './1-logger.js'
+const log = console.log
+
+log('message from log')
+log('message from log2')
