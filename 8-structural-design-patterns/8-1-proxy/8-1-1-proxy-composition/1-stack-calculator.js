f79808dfee593ec66d5b52561c13b70b4d57e053
승종 배 <succeeding@nexon.co.kr>
2022-10-14 13:48:00 +0900

    + 8-1-1 p305

---
+++ 8-structural-design-patterns/8-1-proxy/8-1-1-proxy-composition/1-stack-calculator.js
@@ -0,0 +1,37 @@
+class StackCalculator {
+  constructor () {
+    this.stack = []
+  }
+
+  putValue(value) {
+    this.stack.push(value)
+  }
+
+  getValue() {
+    return this.stack.pop()
+  }
+
+  peekValue() {
+    return this.stack[this.stack.length - 1]
+  }
+
+  clear() {
+    this.stack = []
+  }
+
+  divide() {
+    const divisor = this.getValue()
+    const dividend = this.getValue()
+    const result = dividend / divisor
+    this.putValue(result)
+    return result
+  }
+
+  multiply() {
+    const multiplicand = this.getValue()
+    const multiplier = this.getValue()
+    const result = multiplier * multiplicand
+    this.putValue(result)
+    return result
+  }
+}
